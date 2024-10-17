3980895b1e70c9e35ce6a9b336692cb50f80b967
BrandonAWong <brandonwong2004@outlook.com>
2024-10-17 11:00:54 -0700

    //

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 rm -rf ~/.config/hypr
-ln -sr ~/.dotfiles/hypr ~//.config/hypr
+ln -sr ~/.dotfiles/hypr ~/.config/hypr
 
 rm -rf ~/.config/dunst
-ln -sr ~/.dotfiles/dunst ~//.config/dunst
+ln -sr ~/.dotfiles/dunst ~/.config/dunst
 
 rm -rf ~/.config/kitty
-ln -sf ~/.dotfiles/kitty ~//.config/kitty
+ln -sf ~/.dotfiles/kitty ~/.config/kitty
 
 rm -rf ~/.config/neofetch
-ln -sr ~/.dotfiles/neofetch ~//.config/neofetch
+ln -sr ~/.dotfiles/neofetch ~/.config/neofetch
 
 rm -rf ~/.config/nvim
-ln -sr ~/.dotfiles/nvim ~//.config/nvim
+ln -sr ~/.dotfiles/nvim ~/.config/nvim
 
 rm -rf ~/.config/ranger
-ln -sr ~/.dotfiles/ranger ~//.config/ranger
+ln -sr ~/.dotfiles/ranger ~/.config/ranger
 
 rm -rf ~/.config/rofi
-ln -sr ~/.dotfiles/rofi ~//.config/rofi
+ln -sr ~/.dotfiles/rofi ~/.config/rofi
 
 rm -rf ~/.config/waybar
-ln -sr ~/.dotfiles/waybar ~//.config/waybar
+ln -sr ~/.dotfiles/waybar ~/.config/waybar
 
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 
