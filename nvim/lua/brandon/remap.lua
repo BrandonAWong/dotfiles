96d04e711ef26c77d56eeb04b18316d485c74b9c
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2024-10-19 21:45:09 -0700

    <C-k><C-d>

--- nvim/lua/brandon/remap.lua
+++ nvim/lua/brandon/remap.lua
@@ -1,12 +1,24 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ff", function() 
-    vim.cmd("Ex") 
+
+vim.keymap.set("n", "<leader>ff", function()
+    vim.cmd("Ex")
 end)
 
-vim.keymap.set("n", "<C-`>", function() 
-    vim.cmd("terminal") 
+vim.keymap.set("n", "<C-`>", function()
+    vim.cmd("terminal")
 end)
+
+-- remove trailing whitespace + save
+vim.keymap.set("n", "<C-k><C-d>", function()
+    local cur_pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd(":%s/\\s\\+$//e")
+    vim.cmd(":nohlsearch")
+    vim.cmd(":w")
+    vim.api.nvim_win_set_cursor(0, cur_pos)
+    vim.notify("Trailing whitespace removed and file saved!", vim.log.levels.INFO)
+end)
+
 
 -- Keybinds to remove to make others smoother
-vim.keymap.del('n', '<C-w><C-D>')  
-vim.keymap.del('n', '<C-w>d')  
+vim.keymap.del('n', '<C-w><C-D>')
+vim.keymap.del('n', '<C-w>d')
