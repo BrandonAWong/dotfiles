4fa914c180c4fe24a86db46cf97ac1cdc45a61a8
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2024-10-20 22:52:18 -0700

    <leader-a>

--- nvim/after/plugin/alpha.lua
+++ nvim/after/plugin/alpha.lua
@@ -1,4 +1,3 @@
-
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
@@ -18,7 +17,7 @@ dashboard.section.header.val = {
 
  dashboard.section.buttons.val = {
    dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+   dashboard.button("i", "  New file", ":ene <BAR> startinsert <CR>"),
    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
    dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
@@ -33,3 +32,4 @@ dashboard.section.buttons.opts.hl = "Keyword"
 dashboard.opts.opts.noautocmd = true
 alpha.setup(dashboard.opts)
 
+vim.keymap.set("n", "<leader>a", "<Cmd>Alpha<CR>")
