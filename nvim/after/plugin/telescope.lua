15e2ada7e62481a3cd0f5bfa93777a164dfac3df
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2024-10-21 13:52:16 -0700

    nvi upd

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,3 +1,16 @@
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules",
+            ".venv",
+            "pycache",
+            ".jpg",
+            ".png",
+            ".svg",
+        }
+    }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-o>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
